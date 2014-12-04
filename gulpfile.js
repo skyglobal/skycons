be4d62b515f6562a98bcb297ee9f9c239eaafca4
Peter Mouland <peter.mouland@gmail.com>
2014-12-04 17:20:33 +0000

    gulpify 1st run

--- gulpfile.js
+++ gulpfile.js
@@ -10,44 +10,59 @@ var concat = require('gulp-concat');
 var run = require("gulp-run");
 var runSequence = require('run-sequence');
 var pkg = require('./package.json');
-
 var flatten = require('gulp-flatten');
+var clean = require('gulp-clean');
 
-var iconfontCSS = require("gulp-iconfont-css")
-    , iconfont = require("gulp-iconfont")
-    , path = require("path");
-
-var paths= {
-    "demo": [
-        "./demo/**/*.html"
-    ],
-    sass: './src/scss',
-    demoSass: './demo/scss',
-    css: './_site/css',
-    assets: './src/assets',
-    siteAssets: './_site/assets'
+var paths = {
+    "site": {
+        root: './_site',
+        js: "./_site/js",
+        css: './_site/css',
+        sass: './_site/scss',
+        fonts: './_site/fonts',
+        icons: './_site/icons',
+        images: './_site/images'
+    },
+    "demo": {
+        root: "./demo",
+        js: "./demo/js",
+        sass: './demo/scss',
+        fonts: './demo/fonts',
+        icons: './demo/icons',
+        images: './demo/images'
+    },
+    source: {
+        root: "./src",
+        js: "./src/js",
+        sass: './src/scss',
+        fonts: './src/fonts',
+        icons: './src/icons',
+        images: './src/images'
+    },
+    dist : {
+        root: "./dist",
+        js: "./dist/js",
+        css: "./dist/css",
+        sass: "./dist/scss",
+        fonts: './dist/fonts'
+    }
 };
 
-paths.sources ={
-    icons: './src/svg/*.svg',
-    fontStylesheet: './src/gulp.scss'
-};
-paths.dist = {
-    fonts: './_site/icons',
-    stylesheets: './_site/icons',
-    fontStylesheet: 'icons.scss'
-};
-paths.sass = paths.siteAssets; //fonts are special!
+gulp.task('pre-build', function(cb){
+    //
+});
 
 gulp.task('sass', function() {
     browserSync.notify('<span style="color: grey">Running:</span> Sass compiling');
-    return gulp.src([paths['sass'] + '/**/*.scss', paths['demoSass'] + '/**/*.scss'])
+    return gulp.src([
+            paths.source['sass'] + '/**/*.scss',
+            paths.demo['sass'] + '/**/*.scss'])
         .pipe(sass({
-            includePaths: ['bower_components','_site'],
+            includePaths: ['bower_components'],
             outputStyle: 'nested'
         }))
         .pipe(autoprefixer())
-        .pipe(gulp.dest(paths.css))
+        .pipe(gulp.dest(paths.site['css']))
         .pipe(browserSync.reload({stream:true}));
 });
 
@@ -56,7 +71,7 @@ gulp.task('bower', function() {
 });
 
 gulp.task('gh-pages', function () {
-    gulp.src("./_site/**/*")
+    gulp.src(paths.site['root'] + "/**/*")
         .pipe(deploy({
             cacheDir: '.tmp'
         })).pipe(gulp.dest('/tmp/gh-pages'));
@@ -72,37 +87,39 @@ gulp.task('browserSync', function() {
     browserSync({
         port: 3456,
         server: {
-            baseDir: "_site"
+            baseDir: paths.site['root']
         }
     });
 });
 
-gulp.task('create-site', function() {
-    gulp.src(['demo/index.html','demo/_includes/*.html'])
-        .pipe(concat('index.html'))
-        .pipe(gulp.dest('_site'))
-});
-
-
+function copyDir(location, fileType){
+    var files = (fileType === 'css') ? '/main.css' : '/**/*';
+    return gulp.src([paths[location][fileType] + files])
+        .pipe(gulp.dest(paths.dist[fileType]));
+}
 gulp.task('create-bower-dist', function() {
-    gulp.src([paths.css + '/skycons.css'])
-        .pipe(gulp.dest(paths.siteAssets));
-
-    gulp.src([paths.siteAssets + '/**/*'])
-        .pipe(flatten())
-        .pipe(gulp.dest('./dist'));
+    copyDir('site', 'css');
+    copyDir('site','sass');
+    copyDir('site','fonts');
+    return copyDir('source','sass');
 
 });
 
-gulp.task('copy-svg', function() {
-    return gulp.src(['./src/svg/*.svg'])
-        .pipe(gulp.dest('./_site/svg/'));
+gulp.task('watch', function() {
+    gulp.watch(paths.site['root'], ['create-site']);
+    gulp.watch([paths.source['sass'] + '/**/*',paths.demo['sass']], ['sass']);
 });
 
+gulp.task('create-site', function() {
+    gulp.src([paths.demo['root'] + '/index.html',
+            paths.demo['root'] +'/_includes/*.html'])
+        .pipe(concat('index.html'))
+        .pipe(gulp.dest(paths.site['root']));
+});
 
 
 gulp.task('build', function(cb) {
-    return runSequence(['copy-svg','create-site','bower'],['sass'],'clean','create-bower-dist',
+    return runSequence('pre-build', ['create-site','bower'],['sass'], 'create-bower-dist',
         cb
     );
 });
@@ -133,13 +150,41 @@ gulp.task('release:gh-pages', function(cb) {
 
 
 
-gulp.task("icons", function(){
-    gulp.src(paths.sources.icons)
+
+
+
+
+
+
+
+
+
+
+
+gulp.task('copy-icons', function() {
+    return gulp.src([paths.source['icons'] + '/*.svg'])
+        .pipe(gulp.dest(paths.site['icons']));
+});
+
+
+gulp.task("icons-fonts", function(){
+    return gulp.src(paths.source.icons + '/**/*.svg')
+        .pipe(iconfont({
+            fontName : "skycons",
+            fixedWidth : false,
+            normalize: true,
+            centerHorizontally: true
+        }))
+        .pipe(gulp.dest(paths.site['fonts']));
+});
+
+gulp.task("icons-css", function(){
+    return gulp.src(paths.source.icons + '/**/*.svg')
         .pipe(iconfontCSS({
             fontName : "skycons",
-            path : paths.sources.fontStylesheet,
-            targetPath : paths.dist.fontStylesheet,
-            fontPath : ""
+            path : paths.source.fontSassTemplate,
+            targetPath : paths.site.fontSassTemplate,
+            fontPath : "../fonts/"
         }))
         .pipe(iconfont({
             fontName : "skycons",
@@ -147,25 +192,80 @@ gulp.task("icons", function(){
             normalize: true,
             centerHorizontally: true
         }))
-        .pipe(gulp.dest(paths.dist.fonts));
+        .pipe(gulp.dest(paths.site['root']));
 });
+
+gulp.task("icons-html", function(){
+    return gulp.src(paths.source.icons + '/**/*.svg')
+        .pipe(iconfontCSS({
+            fontName : "skycons",
+            path : paths.source.fontHtmlTemplate,
+            targetPath : paths.site.fontHtmlTemplate,
+            fontPath : "../fonts/"
+        }))
+        .pipe(iconfont({
+            fontName : "skycons",
+            fixedWidth : false,
+            normalize: true,
+            centerHorizontally: true
+        }))
+        .pipe(gulp.dest(paths.site['root']));
+});
+
+gulp.task("icons-clean", function(){
+    return gulp.src([
+            paths.site['root'] + '/*.eot',
+            paths.site['root'] + '/*.svg',
+            paths.site['root'] + '/*.ttf',
+            paths.site['root'] + '/*.woff'
+    ], {read: false})
+        .pipe(clean());
+});
+
 gulp.task('icons-sass', function(){
 
-    return gulp.src(['./_site/icons/**/*.scss',paths['demoSass'] + '/**/*.scss'])
+    return gulp.src([
+        paths.site['icons'] + '/**/*.scss',
+        paths.site['sass'] + '/**/*.scss',
+        paths.demo['sass'] + '/**/*.scss'
+        ])
         .pipe(sass({
             includePaths: ['bower_components','_site'],
             outputStyle: 'nested'
         }))
         .pipe(autoprefixer())
-        .pipe(gulp.dest('./_site/icons/'))
+        .pipe(gulp.dest(paths.site['css']))
         .pipe(browserSync.reload({stream:true}));
 
 });
 
-gulp.task('skycons', function(callback) {
+gulp.task("skycons", function(callback){
     return runSequence(
-        'icons',
-        ['icons-sass'],
+        'icons-fonts',
+        'icons-css',
+        'icons-html',
+        'icons-sass',
+        'icons-clean',
         callback
     );
 });
+
+gulp.task('sass', function() {
+    //
+});
+
+gulp.task('pre-build', function(cb){
+    return runSequence('copy-icons','skycons', cb);
+});
+
+
+
+var iconfontCSS = require("./node_forks/gulp-iconfont-css")
+    , iconfont = require("gulp-iconfont")
+    , path = require("path");
+
+paths.source.fontSassTemplate = paths.source.root + '/sass-template.scss';
+paths.source.fontHtmlTemplate = paths.source.root + '/html-template.html';
+paths.site.fontSassTemplate = 'scss/main.scss';
+paths.site.fontHtmlTemplate = 'index.html';
+
